41fc624222fa72d0cb357033d90a79a1020468c3
NatalioStefanoMalagrino <malagrinonatalio@gmail.com>
2023-02-01 22:33:03 -0300

    PreEntrega1Malagrino

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import CartWidget from "../CartWidget/CartWidget";
 
 export const Navbar = () => {
   return (
-    <div
-      style={{ display: "flex", width: "100%", paddingLeft: "2%", justifyContent: "space-between", alignItems: "center", backgroundColor: "black", color: "white" }}>
+    <div style={{ display: "flex", width: "100%", paddingLeft: "2%", justifyContent: "space-between", alignItems: "center", backgroundColor: "black", color: "white" }}>
       <img src="logo1.png" alt="logo" style={{ width: "2%", heigth: "2%" }} />
+      <h1>VALENTINE BURGERS</h1>
       <ul style={{ display: "flex", listStyle: "none" }}>
         <li style={{ padding: "15px" }}>
             <a style={{textDecoration: "none", color: "white", fontWeight: "bold"}} href="./index.html">NOSOTROS</a>
